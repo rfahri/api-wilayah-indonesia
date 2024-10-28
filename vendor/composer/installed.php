217373d992ce81b17eeda97eac9a35ab666da50d
Fahri Renngur <fahrirenngur@outlook.com>
2024-10-29 08:56:58 +0900

    update branch

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'emsifa/api-wilayah',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'ebc5151c762d46d89c0679f5d61e6b5bb6db8c40',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'emsifa/api-wilayah' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'ebc5151c762d46d89c0679f5d61e6b5bb6db8c40',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
